19b87d905f69ac5369aeda584335bc730a569437
Omixxx <xxcortex99@gmail.com>
2022-11-30 18:25:37 +0100

    add expense ad incomes page and re-organized the project structure

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,10 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Citrus',
-  webDir: 'build',
-  bundledWebRuntime: false
+  appId: "io.ionic.starter",
+  appName: "citrus",
+  webDir: "build",
+  bundledWebRuntime: false,
 };
 
 export default config;
---
+++ src/pages/expenditure_and_incomes/ExprenditureAndIncome.tsx
@@ -0,0 +1,33 @@
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonButton,
+} from "@ionic/react";
+import List from "../../components/List";
+import "./ExpenditureAndIncome.css";
+
+// todo: fetch from backend
+let myList: string[] = ["cip", "ciop"];
+
+const ExpenditureAndIncome: React.FC = () => {
+  return (
+    <IonPage>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">income</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <List arr={myList}></List>
+        <div className="center">
+          <IonButton>il bottone</IonButton>
+        </div>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default ExpenditureAndIncome;
