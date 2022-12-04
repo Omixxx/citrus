993e3f0c484e89425fbdf54c7645b8222dc9082f
Omixxx <xxcortex99@gmail.com>
2022-12-04 13:38:31 +0100

    created backend whit dockerized db, and added prisma ORM connetion and seeding

---
+++ backend/prisma/seed.ts
@@ -0,0 +1,41 @@
+import { db } from "../src/utils/db.server";
+
+type User = {
+  username: string;
+};
+
+//tutto questo casino per rendere il mapping asincrono. ogni procedura di creazione deve essere
+//asincrona.
+async function main() {
+  const entrys = await Promise.all(
+    getUsers().map(async (user) => {
+      return await db.user.create({
+        data: {
+          username: user.username,
+        },
+      });
+    })
+  );
+  console.log(entrys);
+}
+
+main()
+  .then(async () => {
+    await db.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await db.$disconnect();
+    process.exit(1);
+  });
+
+function getUsers(): Array<User> {
+  return [
+    {
+      username: "tut",
+    },
+    {
+      username: "ki",
+    },
+  ];
+}
