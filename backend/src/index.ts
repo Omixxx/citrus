e9b38c0457a700bcc2493015ca37b4ff1774852a
Omixxx <xxcortex99@gmail.com>
2022-12-13 13:31:23 +0100

    add html-status library

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,23 @@
 import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
+import status from "http-status";
 
 dotenv.config();
 
-if (!process.env.PORT) {
+if (!process.env.SERVER_PORT) {
   process.exit(1);
 }
 
-const SERVERPORT: number = parseInt(process.env.PORT as string, 10);
+const SERVER_PORT: number = parseInt(process.env.SERVER_PORT as string, 10);
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.listen(SERVERPORT, () => {
-  console.log(`Listening on port ${SERVERPORT}`);
+app.listen(SERVER_PORT, () => {
+  console.log(`Listening on port ${SERVER_PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.status(status.OK).send("tut-ki");
 });
