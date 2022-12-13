092cbff8bd9362d34411ed81ba03906d71189fed
Omixxx <xxcortex99@gmail.com>
2022-12-13 12:17:33 +0100

    server port constant rename

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,11 +8,11 @@ if (!process.env.PORT) {
   process.exit(1);
 }
 
-const PORT: number = parseInt(process.env.PORT as string, 10);
+const SERVERPORT: number = parseInt(process.env.PORT as string, 10);
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+app.listen(SERVERPORT, () => {
+  console.log(`Listening on port ${SERVERPORT}`);
 });
